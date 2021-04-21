648b06dd38da5974bebe3b5ea13353b36f13a56d
Sachin <sachin.sampathkumar@care.com>
2021-04-21 16:46:28 +0530

    Initial Commit

---
+++ DayProgress/DayProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayProgressApp.swift
+//  DayProgress
+//
+//  Created by Sachin Sampathkumar on 21/04/21.
+//
+
+import SwiftUI
+
+@main
+struct DayProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
